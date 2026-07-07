490db67a81b78ed748ec1a8db10bb9e1c9960c91
Gustavo Deola <gustavodeola@gmail.com>
2026-07-07 17:54:30 -0300

    feat: implement local SQLite connection with auto-migrate

--- internal/infrastructure/database/connection.go
+++ internal/infrastructure/database/connection.go
@@ -1,1 +1,25 @@
 package database
+
+import (
+	"log"
+	"transactionhub/internal/infrastructure/repository"
+
+	"github.com/glebarez/sqlite"
+	"gorm.io/gorm"
+)
+
+func NewConnection() *gorm.DB {
+	db, err := gorm.Open(sqlite.Open("transactionhub.db"), &gorm.Config{})
+
+	if err != nil {
+		log.Fatal("Failed to connect to database: ", err)
+	}
+
+	err = db.AutoMigrate(&repository.AccountModel{}, &repository.TransactionModel{})
+
+	if err != nil {
+		log.Fatal("Failed to create the tables: ", err)
+	}
+
+	return db
+}
