f64efeb678253a70526f7d9a629755e9af4d48e1
Gustavo Deola <gustavodeola@gmail.com>
2026-07-07 17:54:56 -0300

    feat: add GORM repositories for Accounts and Transactions

--- internal/infrastructure/repository/account_repository.go
+++ internal/infrastructure/repository/account_repository.go
@@ -1,1 +1,61 @@
 package repository
+
+import (
+	"context"
+	"transactionhub/internal/domain/account"
+
+	"gorm.io/gorm"
+)
+
+type GormAccountRepository struct {
+	db *gorm.DB
+}
+
+func NewGormAccountRepository(db *gorm.DB) *GormAccountRepository {
+	return &GormAccountRepository{db: db}
+}
+
+type AccountModel struct {
+	ID              string `gorm: "primary_key; column:id"`
+	Balance         int64  `gorm: "column:balance"`
+	ReservedBalance int64  `gorm: "column:reserved_balance"`
+	CreditLimit     int64  `gorm: "column:credit_limit"`
+	Status          string `gorm: "column:status"`
+}
+
+func (AccountModel) TableName() string {
+	return "accounts"
+}
+
+func (repo *GormAccountRepository) FindByID(ctx context.Context, id string) (*account.Account, error) {
+
+	var accountModel AccountModel
+
+	err := repo.db.WithContext(ctx).Where("id = ?", id).First(&accountModel).Error
+
+	if err != nil {
+		return nil, err
+	}
+
+	return &account.Account{
+		ID:              accountModel.ID,
+		Balance:         accountModel.Balance,
+		ReservedBalance: accountModel.ReservedBalance,
+		CreditLimit:     accountModel.CreditLimit,
+		Status:          account.Status(accountModel.Status),
+	}, nil
+}
+
+func (r *GormAccountRepository) Save(ctx context.Context, account *account.Account) error {
+
+	accountModel := AccountModel{
+
+		ID:              account.ID,
+		Balance:         account.Balance,
+		ReservedBalance: account.ReservedBalance,
+		CreditLimit:     account.CreditLimit,
+		Status:          string(account.Status),
+	}
+
+	return r.db.WithContext(ctx).Save(accountModel).Error
+}
--- internal/infrastructure/repository/transaction_repository.go
+++ internal/infrastructure/repository/transaction_repository.go
@@ -1,1 +1,96 @@
 package repository
+
+import (
+	"context"
+	"encoding/json"
+	"errors"
+	"transactionhub/internal/domain/transaction"
+
+	"gorm.io/gorm"
+)
+
+type GormTransactionRepository struct {
+	db *gorm.DB
+}
+
+func NewGormTransactionRepository(db *gorm.DB) *GormTransactionRepository {
+	return &GormTransactionRepository{db: db}
+}
+
+type TransactionModel struct {
+	ID           string `gorm: "primary_key; column:id"`
+	AccountID    string `gorm: "column:account_id"`
+	Amount       int64  `gorm: "column:amount"`
+	Currency     string `gorm: "column:currency"`
+	ReferenceID  string `gorm: "column:reference_id"`
+	Status       string `gorm: "column:status"`
+	Metadata     []byte `gorm: "column:metadata"`
+	ErrorMessage string `gorm: "column:error_message"`
+}
+
+func (TransactionModel) TableName() string {
+	return "transactions"
+}
+
+func (r *GormTransactionRepository) Save(ctx context.Context, tx *transaction.Transaction) error {
+	metaBytes, _ := json.Marshal(tx.Metadata)
+
+	var errorMessage string
+	if tx.ErrorMessage != nil {
+		errorMessage = *tx.ErrorMessage
+	}
+
+	transactionModel := &TransactionModel{
+		ID:           tx.ID,
+		AccountID:    tx.AccountID,
+		Amount:       tx.Amount,
+		Currency:     tx.Currency,
+		ReferenceID:  tx.ReferenceID,
+		Status:       string(tx.Status),
+		Metadata:     metaBytes,
+		ErrorMessage: errorMessage,
+	}
+
+	return r.db.WithContext(ctx).Save(&transactionModel).Error
+}
+
+func (r *GormTransactionRepository) FindReferenceByID(ctx context.Context, referenceID string) (*transaction.Transaction, error) {
+
+	var transactionModel TransactionModel
+
+	err := r.db.WithContext(ctx).Where("reference_id = ?", referenceID).First(&transactionModel).Error
+	if err != nil {
+		if errors.Is(err, gorm.ErrRecordNotFound) {
+			return nil, nil
+		}
+		return nil, err
+	}
+	return r.mapToDomain(transactionModel), nil
+}
+
+//Auxiliar
+
+func (r *GormTransactionRepository) mapToDomain(transactionModel TransactionModel) *transaction.Transaction {
+	var metaData map[string]interface{}
+	if len(transactionModel.Metadata) > 0 {
+		_ = json.Unmarshal(transactionModel.Metadata, &metaData)
+	}
+
+	var errMsgPtr *string
+	if transactionModel.ErrorMessage != "" {
+
+		msg := transactionModel.ErrorMessage
+		errMsgPtr = &msg
+	}
+
+	return &transaction.Transaction{
+		ID:           transactionModel.ID,
+		AccountID:    transactionModel.AccountID,
+		Amount:       transactionModel.Amount,
+		Currency:     transactionModel.Currency,
+		ReferenceID:  transactionModel.ReferenceID,
+		Status:       transaction.Status(transactionModel.Status),
+		Metadata:     metaData,
+		ErrorMessage: errMsgPtr,
+	}
+}
