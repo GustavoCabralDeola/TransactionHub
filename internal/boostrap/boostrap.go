7264f3cf66838fc2cddf10d47104670d3fab4235
Gustavo Deola <gustavodeola@gmail.com>
2026-07-08 22:54:49 -0300

    feat: adding boostrap initializer

--- internal/boostrap/boostrap.go
+++ internal/boostrap/boostrap.go
@@ -1,1 +1,40 @@
 package boostrap
+
+import (
+	"log"
+	"transactionhub/internal/api"
+	"transactionhub/internal/api/routes"
+	"transactionhub/internal/application/commands"
+	"transactionhub/internal/infrastructure/repository"
+
+	"github.com/gin-gonic/gin"
+	"gorm.io/gorm"
+)
+
+type Application struct {
+	Router *gin.Engine
+}
+
+func Build(db *gorm.DB) *Application {
+	accountRepo := repository.NewGormAccountRepository(db)
+	transactionRepo := repository.NewGormTransactionRepository(db)
+
+	transferHandler := commands.NewTransferHandler(accountRepo, transactionRepo)
+
+	accountController := api.NewAccountController(accountRepo)
+	transactionController := api.NewTransactionController(transferHandler)
+
+	router := routes.SetupRouter(accountController, transactionController)
+
+	return &Application{
+		Router: router,
+	}
+
+}
+
+func (app *Application) Start(port string) {
+	log.Printf("Server started on port %s...", port)
+	if err := app.Router.Run(port); err != nil {
+		log.Fatalf("Failed to start server: %v", err)
+	}
+}
