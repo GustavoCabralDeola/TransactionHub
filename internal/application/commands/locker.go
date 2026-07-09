1d4f1fe88abed7d2db614dafbbe7eecca64f85a8
Gustavo Deola <gustavodeola@gmail.com>
2026-07-09 17:32:34 -0300

    feat: Lockers for transfer and account

--- internal/application/commands/locker.go
+++ internal/application/commands/locker.go
@@ -1,1 +1,39 @@
 package commands
+
+import "sync"
+
+var (
+	accountLocks sync.Map
+)
+
+// LockAccount cria um bloqueio (Mutex) para uma conta específica
+func LockAccount(accountID string) func() {
+	val, _ := accountLocks.LoadOrStore(accountID, &sync.Mutex{})
+	mu := val.(*sync.Mutex)
+
+	mu.Lock()
+
+	return func() {
+		mu.Unlock()
+	}
+}
+
+// LockTransferAccounts trava duas contas simultaneamente.
+func LockTransferAccounts(sourceID, destinationID string) func() {
+	if sourceID == destinationID {
+		return LockAccount(sourceID)
+	}
+
+	first, second := sourceID, destinationID
+	if first > second {
+		first, second = second, first
+	}
+
+	unlockFirst := LockAccount(first)
+	unlockSecond := LockAccount(second)
+
+	return func() {
+		unlockSecond()
+		unlockFirst()
+	}
+}
